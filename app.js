0f8349ab4cee204ec9474f45da3d914a9a863abc
jlee474 <jlee474@gmail.com>
2022-01-13 22:49:16 -0500

    basic setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+// express() creates an 'express application'. 
+const app = express(); 
+
+// sets up a 'development server' using the listen method. 3000 is the port number
+app.listen(3000); 
+
